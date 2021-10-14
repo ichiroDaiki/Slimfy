53ec225b02a43f8a8ff91180cbce984b462998cb
JostickDev <jostick516@gmail.com>
2021-10-14 14:17:15 -0500

    Improve code

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,14 +7,18 @@ use rand::Rng;
 use rand::seq::SliceRandom;
 
 
+fn shuffle(x: &mut String) -> String{
+      let mut rng = rand::thread_rng();
+      let mut bytes = x.clone().into_bytes();
+      bytes.shuffle(&mut rng);
+      let shuffle_password = String::from_utf8(bytes).unwrap();
+      shuffle_password
+}
+
 #[tauri::command]
 fn _generator() -> String{
  
   let mut rng = rand::thread_rng();
-  let mut _posicion_abc = 0;
-  let mut _posicion_numeros = 0;
-  let mut _posicion_caracteres = 0;
- 
   let _abecedario = [
       "A", "B", "C", "D", "E", "F", "G",
       "H", "I", "J", "K", "L", "M", "N",
@@ -31,54 +35,31 @@ fn _generator() -> String{
   "-", "*", "?", "!", "@", "#", "",
   "$", "=", ".", ";", ":", ""];
 
-  let mut _password = String::new();
-
-  let mut _final_result = String::new();
-
-
-
+  let mut generate_password : String = String::new();
   let mut _i: u8 = 0;
 
-
   for _i in 0..3 {
-      _posicion_abc = rng.gen_range(0..26);
-      _posicion_numeros = rng.gen_range(0..9);
 
-      let _abc_mayuscula_valor: &str = _abecedario[_posicion_abc];
-      let _abc_minuscula_valor = _abecedario[_posicion_numeros].to_lowercase();
-      let _abc_minuscula_valor= _abc_minuscula_valor.as_str();
+      let _abc_mayuscula_valor: &str = _abecedario[rng.gen_range(0..26)];
+      let _abc_minuscula_valor: String = _abecedario[rng.gen_range(0..9)].to_lowercase();
+      let _abc_minuscula_valor: &str = _abc_minuscula_valor.as_str();
 
-      let _numero_valor: &str = _numeros[_posicion_numeros];
+      let _numero_valor: &str = _numeros[rng.gen_range(0..9)];
 
-      _password.push_str(_abc_mayuscula_valor);
-      _password.push_str(_abc_minuscula_valor);
-      _password.push_str(_numero_valor);
-
-      if _i == 0 {
-          _posicion_caracteres = rng.gen_range(0..13);
-          let _caracter_valor: &str = _caracteres[_posicion_caracteres];
-          _password.push_str(_caracter_valor);
-
-      }
-
-      if _i == 2 {
-          _posicion_caracteres = rng.gen_range(0..13);
-          let _caracter_valor: &str = _caracteres[_posicion_caracteres];
-          _password.push_str(_caracter_valor);
-
-      }
-
-      let mut bytes = _password.to_string().into_bytes();
-      bytes.shuffle(&mut rng);
-      _final_result = String::from_utf8(bytes).unwrap();
-     
+      generate_password.push_str(_abc_mayuscula_valor);
+      generate_password.push_str(_abc_minuscula_valor);
+      generate_password.push_str(_numero_valor);
 
   }
+  
+  let _caracter_valor: &str = _caracteres[rng.gen_range(0..13)];
+  generate_password.push_str(_caracter_valor);
+  let contra : String = shuffle(&mut generate_password);
 
-  _final_result
-
+  contra
 
 }
+
 
 fn main() {
   tauri::Builder::default()
